59b44465deb4bde408d119e71ebc1bece1e47328
RajputanaRoyality <vaibhavrksingh6207@gmail.com>
2025-09-24 01:04:00 +0530

    eee

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,46 +2,47 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Footer = () => {
-  return (
-    <div className='mt-35'>
-        <div className='flex flex-row mb-[-270px]'>
-        <img src={assets.logo} className='md-5 mb-[-140px] w-15 h-13' alt=""/>
-        <h1 className='font-bold text-xl mt-3'>Vaiso Store</h1>
-        </div>
-        
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-            <div>
-            
-            <p className=' text-gray-600'>
-            Always bringing newest and trendy fashion to you, always with you to care and support.
-            </p>
+    return (
+        <div className='mt-35'>
+            <div className='flex mb-[-270px]'>
+                <img src={assets.logo} className='md-5 mb-[-140px] w-15 h-13 ' alt="" />
+                <h1 className='font-bold text-xl mt-3  ms:[2rem]'>Vaiso Store</h1>
             </div>
-            
 
-            <div>
-            <p className='text-xl font-medium mb-5'>Company</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy Policy</li>
-            </ul>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+                <div>
+
+                    <p className=' text-gray-600'>
+                        Always bringing newest and trendy fashion to you, always with you to care and support.
+                    </p>
+                </div>
+
+
+                <br />
+                <div>
+                    <p className='text-xl font-medium mb-5'>Company</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>7979096883</li>
+                        <li>contactforeveru@gmail.com</li>
+                    </ul>
+                </div>
+
+            </div>
+
+
         </div>
 
-        <div>
-            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>7979096883</li>
-                <li>contactforeveru@gmail.com</li>
-            </ul>
-        </div>
-
-        </div>
-
-        
-    </div>
-    
-  )
+    )
 }
 
 export default Footer
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,29 +17,29 @@ const Navbar = () => {
 
     return (
         <div className='flex items-center justify-between py-4 font-medium bg-gray-200 z-1'>
-            <Link to='/'><img src={assets.logo} alt="" className='ml-10 h-15 w-20' /><h2 className='ml-10 font-bold text-red-900'>Vaiso Store</h2></Link>
+            <Link to='/'><img src={assets.logo} alt="" className='ml-10 h-15 w-20' /><h2 className='ml-10 font-bold text-red-900 ms:[2rem]'>Vaiso Store</h2></Link>
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <div className='flex flex-row gap-10'>
-                <NavLink to='/' className='flex flex-col items-center gap-1 '>
-                    <p>HOME</p>
-                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
-                </NavLink>
+                    <NavLink to='/' className='flex flex-col items-center gap-1 '>
+                        <p>HOME</p>
+                        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                    </NavLink>
 
-                <NavLink to='/collection' className='flex flex-col items-center gap-1 '>
-                    <p>COLLECTION</p>
-                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
-                </NavLink>
+                    <NavLink to='/collection' className='flex flex-col items-center gap-1 '>
+                        <p>COLLECTION</p>
+                        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                    </NavLink>
 
-                <NavLink to='/about' className='flex flex-col items-center gap-1 '>
-                    <p>ABOUT</p>
-                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
-                </NavLink>
+                    <NavLink to='/about' className='flex flex-col items-center gap-1 '>
+                        <p>ABOUT</p>
+                        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                    </NavLink>
 
-                <NavLink to='/contact' className='flex flex-col items-center gap-1 '>
-                    <p>CONTACT</p>
-                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
-                </NavLink>
+                    <NavLink to='/contact' className='flex flex-col items-center gap-1 '>
+                        <p>CONTACT</p>
+                        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                    </NavLink>
                 </div>
 
             </ul>
@@ -54,7 +54,7 @@ const Navbar = () => {
                         <div className='group-hover:block hidden flex dropdown-menu right-0 pt-4'>
                             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                                 <p className='cursor-pointer hover:text-black'>My Profile</p>
-                                <p onClick={()=>navigate('/orders')} className='cursor-pointer hover:text-black'>Order</p>
+                                <p onClick={() => navigate('/orders')} className='cursor-pointer hover:text-black'>Order</p>
                                 <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
                             </div>
                         </div>
